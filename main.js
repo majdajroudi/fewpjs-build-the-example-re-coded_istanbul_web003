385deddffe9fc99ff4b405bfea4e249cd7c786cd
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-11-01 00:42:03 +0100

    commit tree f10efb10c03b97a051a387446cf2b3b663dbeb4d

--- main.js
+++ main.js
@@ -3,8 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likes = document.querySelectorAll(".like")
+const errorModal = document.querySelector("#errorModal")
+const heartElements = document.querySelectorAll(".like-glyph")
+heartElements.forEach(element => {
+  element.addEventListener("click",(e) => {
+    e.target.classList.remove("actiavted-heart")
+  })
+})
 
+likes.forEach(like => {
+  like.addEventListener("click", mimicServerCall()
+  .then(data => {
+    heartElements.forEach(element => {
+      element.classList.add("activated-heart")
+    })
+  })
+  .catch(error => {
+    errorModal.classList.remove("hidden")
+    errorModal.innerText = error
+    setTimeout(() => errorModal.classList.add("hidden"),5000)
+  })
+  )
+})
 
+heartElements.forEach(element => {
+  element.addEventListener("click",() => {
+    element.classList.remove("actiavted-heart")
+  })
+})
 
 
 //------------------------------------------------------------------------------
